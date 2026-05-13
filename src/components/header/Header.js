54ade044bd18435a1d6f0477a5dbd744ba4dda27
Aakash Vajpayee <akashbajpai167@gmail.com>
2026-05-13 23:36:56 +0530

    fix: Change in Header section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -60,18 +60,7 @@ class Header extends Component {
                   Education
                 </NavLink>
               </li>
-              <li>
-                <NavLink
-                  to="/experience"
-                  tag={Link}
-                  activeStyle={{ fontWeight: "bold" }}
-                  style={{ color: theme.text }}
-                  onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                  onMouseOut={(event) => onMouseOut(event)}
-                >
-                  Experience
-                </NavLink>
-              </li>
+              
               <li>
                 <NavLink
                   to="/projects"
@@ -82,6 +71,18 @@ class Header extends Component {
                   onMouseOut={(event) => onMouseOut(event)}
                 >
                   Projects
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/experience"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                  style={{ color: theme.text }}
+                  onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
+                  onMouseOut={(event) => onMouseOut(event)}
+                >
+                  Experience
                 </NavLink>
               </li>
               {/* <li>
